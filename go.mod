1803a656b479e19b040950904fc77cd1f22177e6
zzzhr-pc <zzzhr@hotmail.com>
2020-03-27 20:40:56 +0800

    add gorm support

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/zzzhr1990/go-gorm-util
+
+go 1.14
+
+require (
+	github.com/jinzhu/gorm v1.9.12
+	github.com/sirupsen/logrus v1.5.0
+)
---
+++ page/pagination.go
@@ -0,0 +1,62 @@
+package page
+
+import (
+	"math"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/jinzhu/gorm"
+)
+
+//Paginator for page use
+type Paginator struct {
+	TotalCount int64 //`json:"total_record"`
+	TotalPage  int32 // `json:"total_page"`
+	Page       int32 //`json:"page"`
+	PageSize   int32 //`json:"prev_page"`
+}
+
+// DoPage ip
+func (p *Paginator) DoPage(table *gorm.DB, list interface{}, order []string) error {
+	//table.Or
+	return Page(table, p, list, order)
+}
+
+//Page so
+func Page(table *gorm.DB, p *Paginator, list interface{}, order []string) error {
+
+	if p.Page < 1 {
+		p.Page = 1
+	}
+	if p.PageSize < 1 {
+		p.PageSize = 20
+	}
+	done := make(chan bool, 1)
+	go countRecords(table, done, &p.TotalCount)
+	offset := (p.Page - 1) * p.PageSize
+	if len(order) > 0 {
+		for _, element := range order {
+			table = table.Order(element)
+		}
+	}
+	err := table.Limit(p.PageSize).Offset(offset).Find(list).Error
+	<-done
+	if err != nil {
+		log.Errorf("Query countRecords %v", err)
+		return err
+	}
+	p.TotalPage = int32(math.Ceil(float64(p.TotalCount) / float64(p.PageSize)))
+	if p.TotalPage < p.Page {
+		p.Page = p.TotalPage
+	}
+
+	return nil
+}
+
+func countRecords(table *gorm.DB, done chan bool, count *int64) {
+	err := table.Count(count).Error
+	if err != nil {
+		log.Errorf("Query countRecords %v", err)
+	}
+	done <- true
+}
